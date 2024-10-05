d6ebdf3d4db9ff97f93955784bc2bd3fa95f3e2f
CristianTambaco <1234cristiant@gmail.com>
2024-10-04 21:20:47 -0500

    Comentario2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("NOMBRE: Cristian Tambaco");
-
+        System.out.println("DIRECCION: Joaquin Mancheno y Calle B");
 
 
     }
